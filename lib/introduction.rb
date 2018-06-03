67804f87cdc8cd3bebbcc186482a93dc1fcfa70e
Peter Alomia <peteralomia@me.com>
2018-06-03 20:16:57 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,5 @@
+#
+lib/introduction.r                                        b
+def                                                       introduction (name)
+  puts "hi, my                                           name is #{name} ."
+end
